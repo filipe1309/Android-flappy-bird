15d6dd165fe9a2079339bd2371310851ec8575fd
filipe1309 <filipe1309@gmail.com>
2017-07-03 22:26:10 -0300

    Create Passaro(bird) class

---
+++ app/src/main/java/br/com/alura/jumper/elements/Passaro.java
@@ -0,0 +1,26 @@
+package br.com.alura.jumper.elements;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import br.com.alura.jumper.graphic.Cores;
+
+/**
+ * Created by filipe1309 on 03/07/17.
+ */
+
+public class Passaro {
+
+    private static final float X = 100;
+    private static final float RAIO = 50;
+    private static final Paint VERMELHO = Cores.getCorDoPassaro();
+    private float altura;
+
+    public Passaro() {
+        this.altura = 100;
+    }
+
+    public void desenhaNoCanvas(Canvas canvas) {
+        canvas.drawCircle(X, altura, RAIO, VERMELHO);
+    }
+}
--- app/src/main/java/br/com/alura/jumper/engine/Game.java
+++ app/src/main/java/br/com/alura/jumper/engine/Game.java
@@ -5,6 +5,8 @@ import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import br.com.alura.jumper.elements.Passaro;
+
 /**
  * Created by filipe1309 on 03/07/17.
  */
@@ -12,9 +14,16 @@ import android.view.SurfaceView;
 public class Game extends SurfaceView implements Runnable {
     private boolean isRunning = true;
     private SurfaceHolder holder = getHolder();
+    private Passaro passaro;
 
     public Game(Context context) {
         super(context);
+
+        inicializaElementos();
+    }
+
+    private void inicializaElementos() {
+        passaro = new Passaro();
     }
 
     @Override
@@ -23,6 +32,7 @@ public class Game extends SurfaceView implements Runnable {
             if (!holder.getSurface().isValid()) continue;
             Canvas canvas = holder.lockCanvas();
             // draw game components
+            passaro.desenhaNoCanvas(canvas);
             holder.unlockCanvasAndPost(canvas);
         }
     }
