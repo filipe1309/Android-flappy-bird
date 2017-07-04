34a385181b92d168e73a46f93422595d63c2a971
filipe1309 <filipe1309@gmail.com>
2017-07-03 22:26:33 -0300

    Create Cores(colors) class

---
+++ app/src/main/java/br/com/alura/jumper/graphic/Cores.java
@@ -0,0 +1,15 @@
+package br.com.alura.jumper.graphic;
+
+import android.graphics.Paint;
+
+/**
+ * Created by filipe1309 on 03/07/17.
+ */
+
+public class Cores {
+    public static Paint getCorDoPassaro() {
+        Paint paint = new Paint();
+        paint.setColor(0xFFFF0000); // ARGB
+        return paint;
+    }
+}
