65b20f01b5a3905cf4c446dbdab126e06f78d31a
filipe1309 <filipe1309@gmail.com>
2017-07-03 21:52:09 -0300

    Create Game engine class

--- app/src/main/java/br/com/alura/jumper/MainActivity.java
+++ app/src/main/java/br/com/alura/jumper/MainActivity.java
@@ -3,11 +3,16 @@ package br.com.alura.jumper;
 import android.app.Activity;
 import android.os.Bundle;
 
+import br.com.alura.jumper.engine.Game;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Game game = new Game(this);
     }
 }
---
+++ app/src/main/java/br/com/alura/jumper/engine/Game.java
@@ -0,0 +1,14 @@
+package br.com.alura.jumper.engine;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+/**
+ * Created by filipe1309 on 03/07/17.
+ */
+
+public class Game extends SurfaceView {
+    public Game(Context context) {
+        super(context);
+    }
+}
